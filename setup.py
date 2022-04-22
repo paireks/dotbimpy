795252d4a3a9f5ed949ff958a1e72fc388331399
paireks <w.radaczynski@gmail.com>
2022-04-22 22:02:29 +0200

    Version update

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name='dotbimpy',
   packages=['dotbimpy'],
-  version='0.0.6',
+  version='0.0.7',
   license='MIT',
   description='Python library for dotbim',
   author='Wojciech',
   author_email='w.radaczynski@gmail.com',
   url='https://github.com/paireks/dotbimpy',
-  download_url='https://github.com/paireks/dotbimpy/archive/refs/tags/v_0_0_6.tar.gz',
+  download_url='https://github.com/paireks/dotbimpy/archive/refs/tags/v_0_0_7.tar.gz',
   keywords=[],
   install_requires=[
           'jsonpickle',
