f2f953ea3d16ba8bbf214f1931d2cd8115b8eee0
paireks <w.radaczynski@gmail.com>
2023-01-31 22:04:01 +0100

    Move tests of adding files to unittests

--- dotbimpy/tests/unittests/test_file.py
+++ dotbimpy/tests/unittests/test_file.py
@@ -119,3 +119,84 @@ def test_init_cubes():
         5, 6, 7
     ]
 
+
+def test_add_pyramid_cubes():
+
+    file_a = create_file_with_pyramid()
+    file_b = create_file_with_cubes()
+
+    file_result = file_a + file_b
+
+    assert file_result.schema_version == file_a.schema_version
+    assert file_result.info == file_a.info
+
+    # Check meshes
+    assert file_result.meshes[0] == file_a.meshes[0]
+    assert file_result.meshes[1].equals_without_mesh_id(file_b.meshes[0])
+    assert file_result.meshes[1].mesh_id == 1
+
+    # Check elements
+    assert file_result.elements[0] == file_a.elements[0]
+    assert file_result.elements[1].equals_without_mesh_id(file_b.elements[0]) and file_result.elements[
+        1].mesh_id == 1
+    assert file_result.elements[2].equals_without_mesh_id(file_b.elements[1]) and file_result.elements[
+        2].mesh_id == 1
+    assert file_result.elements[3].equals_without_mesh_id(file_b.elements[2]) and file_result.elements[
+        3].mesh_id == 1
+
+
+def test_add_cubes_pyramid():
+
+    file_a = create_file_with_cubes()
+    file_b = create_file_with_pyramid()
+
+    file_result = file_a + file_b
+
+    assert file_result.schema_version == file_a.schema_version
+    assert file_result.info == file_a.info
+    assert file_result.meshes[1].mesh_id == 1
+
+    # Check meshes
+    assert file_result.meshes[0] == file_a.meshes[0]
+    assert file_result.meshes[1].equals_without_mesh_id(file_b.meshes[0])
+
+    # Check elements
+    assert file_result.elements[0] == file_a.elements[0]
+    assert file_result.elements[1] == file_a.elements[1]
+    assert file_result.elements[2] == file_a.elements[2]
+    assert file_result.elements[3].equals_without_mesh_id(file_b.elements[0]) and file_result.elements[3].mesh_id == 1
+
+
+def test_add_walls_truss__check_if_originals_changed():
+    file_a = File.read("test_files/WallsWithBeams.bim")
+    file_b = File.read("test_files/Truss.bim")
+    file_a_copy = copy.deepcopy(file_a)
+    file_b_copy = copy.deepcopy(file_b)
+
+    file_result = file_a + file_b
+
+    assert file_a == file_a_copy
+    assert file_b == file_b_copy
+
+
+def test_add_walls_truss():
+    file_a = File.read("test_files/WallsWithBeams.bim")
+    file_b = File.read("test_files/Truss.bim")
+
+    file_result = file_a + file_b
+
+    assert file_result.schema_version == file_a.schema_version
+    assert file_result.info == file_a.info
+
+    # Check meshes
+    assert file_result.meshes[0] == file_a.meshes[0]
+    assert file_result.meshes[1] == file_a.meshes[1]
+    for i in range(5):
+        assert file_result.meshes[i+2].equals_without_mesh_id(file_b.meshes[i])
+
+    # Check elements
+    for i in range(7):
+        assert file_result.elements[i] == file_a.elements[i]
+
+    for i in range(7, len(file_result.elements)):
+        assert file_result.elements[i].equals_without_mesh_id(file_b.elements[i-7])
